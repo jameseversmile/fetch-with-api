4d6a24866bc283d70a881a21842139ab8edb3e2a
James Idris <36175043+jameseversmile@users.noreply.github.com>
2025-06-15 21:55:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-let movies = fetch('https://api.themoviedb.org/3/movie/popular?api_key=Api key a')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
+// let movies = fetch('https://api.themoviedb.org/3/movie/popular?api_key=b502736b7dc66230d5d6deb21358a72a')
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error('There was a problem with the fetch operation:', error);
+//     });
+
+
+let movieByPopularity = async() => {
+    let response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=b502736b7dc66230d5d6deb21358a72a');
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    let data = await response.json(); 
+    console.log(data);
+
+    let parentDiv = document.getElementsByClassName('popularMovies')[0];
+    
+    data.results.forEach(movie => {
+        let movielist = document.createElement('div');
+        movielist.className = 'movielist';
+        let movieImg = document.createElement('img');
+        let movieTitle = document.createElement('h3');
+    
+        movieImg.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        movieTitle.textContent = movie.title;
+
+        movielist.appendChild(movieImg);
+        movielist.appendChild(movieTitle);
+        parentDiv.appendChild(movielist);
     });
+}
+
+movieByPopularity();
